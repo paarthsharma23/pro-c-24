9f766ec3819967dcaa21be0f82984957457d9973
paarthsharma23 <trender2700@gmail.com>
2020-12-08 11:29:46 +0530

    hello this is my new project

---
+++ paper.js
@@ -0,0 +1,25 @@
+class Paper {
+constructor(x,y){
+    var options ={
+        'isStatic':false,
+        'restitution':0.3,
+        'friction':0.5,
+        'density':1.2
+    }
+this.img = loadImage("crumpled.png");
+this.radius=70;
+this.body = Bodies.circle(x,y,this.radius,options);
+World.add(world,this.body);
+}
+
+display(){
+push();
+var pos = this.body.position ; 
+translate(pos.x,pos.y);
+imageMode(CENTER);
+// fill("white");
+image(this.img,0,0,this.radius,70);
+pop();
+}
+
+};
---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+  rectMode(CENTER);
+  createCanvas(1350, 500);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    
+    this.paper = new Paper(50,200); 
+    this.ground = new Ground(675,450,1350,20);
+    this.db1 = new dustBin(1100,430,200,20);
+    this.db2 = new dustBin(990,365,20,150);
+    this.db3 = new dustBin(1200,365,20,150);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  this.ground.display();
+  this.paper.display();  
+  this.db1.display();
+  this.db2.display();
+  this.db3.display();
+  drawSprites(); 
+}
+
+function keyPressed (){
+  
+  if( keyCode === UP_ARROW ){
+   Matter.Body.applyForce(this.paper.body,this.paper.body.position,{x:1200,y:-1100})
+  }
+  
+}
+
+// function keyPressed (){
+  
+// }
+
+
+
